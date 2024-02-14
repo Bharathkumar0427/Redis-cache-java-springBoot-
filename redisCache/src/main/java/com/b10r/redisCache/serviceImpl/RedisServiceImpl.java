8300d83dc23d0b674b4746495f38d3966d6a0685
Bharathkumar0427 <bharathcode1604@gmail.com>
2024-02-15 00:57:35 +0530

    adding serviceImpl

--- redisCache/src/main/java/com/b10r/redisCache/serviceImpl/RedisServiceImpl.java
+++ redisCache/src/main/java/com/b10r/redisCache/serviceImpl/RedisServiceImpl.java
@@ -1,7 +1,11 @@
 package com.b10r.redisCache.serviceImpl;
 
+import org.springframework.stereotype.Service;
+
 import com.b10r.redisCache.service.RedisService;
 
+
+@Service
 public class RedisServiceImpl implements RedisService{
 
 }
